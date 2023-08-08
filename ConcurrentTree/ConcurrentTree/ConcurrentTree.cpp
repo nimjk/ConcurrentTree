7645db9b044863ef2ae8567d10758b8f5107e02c
nimjk <nimjk@naver.com>
2023-08-08 10:47:29 +0900

    C++ concurrency 파이계산프로그램-VS2022용

--- ConcurrentTree/ConcurrentTree/ConcurrentTree.cpp
+++ ConcurrentTree/ConcurrentTree/ConcurrentTree.cpp
@@ -1,20 +1,47 @@
-﻿// ConcurrentTree.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
-//
+﻿#include <iostream>
+#include <thread>
+#include <random>
+#include <atomic>
 
-#include <iostream>
+// 원 내부에 점이 있는지 검사하고 카운트를 증가시키는 함수
+void monteCarloPiCalculation(std::atomic<int>& insideCircleCount, int totalPoints) {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_real_distribution<> dis(-1.0, 1.0);
 
-int main()
-{
-    std::cout << "Hello World!\n";
+    int insideCount = 0;
+    for (int i = 0; i < totalPoints; ++i) {
+        double x = dis(gen);
+        double y = dis(gen);
+        if (x * x + y * y <= 1) {
+            insideCount++;
+        }
+    }
+
+    insideCircleCount += insideCount;
 }
 
-// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
-// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
+int main() {
+    const int numThreads = 4;
+    const int totalPoints = 1000000;  // 시뮬레이션할 전체 점의 수
+    std::atomic<int> insideCircleCount(0);
 
-// 시작을 위한 팁: 
-//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
-//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
-//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
-//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
-//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
-//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
+    std::thread threads[numThreads];
+
+    // 각 스레드를 시작
+    for (int i = 0; i < numThreads; ++i) {
+        threads[i] = std::thread(monteCarloPiCalculation, std::ref(insideCircleCount), totalPoints / numThreads);
+    }
+
+    // 모든 스레드의 완료를 대기
+    for (int i = 0; i < numThreads; ++i) {
+        threads[i].join();
+    }
+
+    // 파이 값 계산
+    double pi = static_cast<double>(insideCircleCount) / totalPoints * 4;
+
+    std::cout << "Estimated Pi: " << pi << std::endl;
+
+    return 0;
+}
